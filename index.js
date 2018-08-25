7e67ee520fff532b03510a7e048a2724fdc8697a
Nicholas Michau <nmichau1@asu.edu>
2018-08-25 18:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string == string.toLowerCase){
-    return "I can't hear you!"
+  if(string === string.toLowerCase){
+    string = "I can't hear you!"
   }
+  return string
+  
 }
